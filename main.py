21da6eaab5ed92a1a64ce21eb03ca27cea372c60
ironman <64022468@qq.com>
2017-12-19 21:59:35 +0800

    modify url

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from urllib import request, parse
 
 values = {"username":"zhao632@tom.com","password":"cz19831006"}
 data = parse.urlencode(values)
-res = request.Request("http://www.baidu.com",data)
+res = request.Request("http://www.csdn.net/login",data)
 response = request.urlopen(res)
 print(response.read())
